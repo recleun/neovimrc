d1b5125e06985261ebfbc6e95076ea4510f74467
recleun <98621222+recleun@users.noreply.github.com>
2024-02-27 01:58:27 +0200

    Fix typo

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,7 +4,7 @@ lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
-  vim.keymap.se('n', '<leader>ca', vim.lsp.buf.code_action, {})
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 end)
 
 -- to learn how to use mason.nvim with lsp-zero
