f2af51f683d78eb82aa207c8fa656bc34a27f599
recleun <98621222+recleun@users.noreply.github.com>
2024-02-27 02:17:47 +0200

    Fix and add plugins

--- lua/recleunfig/packer.lua
+++ lua/recleunfig/packer.lua
@@ -38,6 +38,8 @@ return require('packer').startup(function(use)
 		}
 	}
 
+	use "lukas-reineke/indent-blankline.nvim"
+
 	use {
 		'catppuccin/nvim',
 		as = 'catppuccin',
--- lua/recleunfig/remaps.lua
+++ lua/recleunfig/remaps.lua
@@ -2,13 +2,15 @@ vim.g.mapleader = " " -- leader, it's a space
 
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex) -- file explorer
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- center cursor when going up and down
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<leader>cs", ':noh<cr>') -- clear search
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
-vim.keymap.set("v", "<C-c>", "\"+y")
+vim.keymap.set("v", "<C-c>", "\"+y") -- copy to system clipboard
 
-vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("x", "<leader>p", "\"_dP") -- prevents overwriting clipboard when replacing text with print
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- changes all occurunces
