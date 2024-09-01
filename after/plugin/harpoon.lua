d6ea12621a74dedce47736e684837a71f171c738
recleun <98621222+recleun@users.noreply.github.com>
2024-09-01 04:36:20 +0300

    Update append() to add()

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,7 +2,7 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<leader>hq", function() harpoon:list():select(1) end)
