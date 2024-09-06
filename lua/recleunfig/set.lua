a466c370fd901455022d8917bb5f943da4a9c9d3
recleun <98621222+recleun@users.noreply.github.com>
2024-09-06 18:04:55 +0300

    Create autocmd to set tab width to 2 on json files

--- lua/recleunfig/set.lua
+++ lua/recleunfig/set.lua
@@ -3,6 +3,13 @@ vim.opt.shiftwidth = 4 -- how many spaces per indentation
 vim.opt.scrolloff = 8 -- check 24:00 in ThePrimeagen video
 vim.opt.expandtab = true
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "json",
+    callback = function()
+        vim.opt.shiftwidth = 2
+    end
+})
+
 vim.opt.number = true -- show numbers
 vim.opt.relativenumber = true -- show relative numbers
 vim.opt.wrap = false
