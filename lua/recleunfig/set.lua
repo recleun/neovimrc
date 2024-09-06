e3bf4a0fafc1a7476073e0495598d7b1e009cb95
recleun <98621222+recleun@users.noreply.github.com>
2024-09-07 00:55:22 +0300

    Add comments to options

--- lua/recleunfig/set.lua
+++ lua/recleunfig/set.lua
@@ -1,8 +1,9 @@
 vim.opt.tabstop = 4 -- how many spaces per tab
 vim.opt.shiftwidth = 4 -- how many spaces per indentation
 vim.opt.scrolloff = 8 -- check 24:00 in ThePrimeagen video
-vim.opt.expandtab = true
+vim.opt.expandtab = true -- tabs convert to spaces when typing them
 
+-- converts shiftwidth to 2 when opening json files
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "json",
     callback = function()
@@ -12,12 +13,12 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.opt.number = true -- show numbers
 vim.opt.relativenumber = true -- show relative numbers
-vim.opt.wrap = false
-vim.opt.guicursor = ""
+vim.opt.wrap = false -- text wraps when it reaches the end of the screen
+vim.opt.guicursor = "" -- sets the cursor to a block
 
-vim.opt.clipboard = ""
-vim.cmd.colorscheme("github_dark")
+vim.opt.clipboard = "" -- separates the system clipboard and nvim's keyboard
+vim.cmd.colorscheme("github_dark") -- sets the theme
 
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- sets a directory to hold undo file that can store days worth undo
+vim.opt.undofile = true -- enables the undo directory feature
 
