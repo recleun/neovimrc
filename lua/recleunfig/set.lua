46686c73c42d20e3aa9589c582fa6ef5c3459963
recleun <98621222+recleun@users.noreply.github.com>
2024-09-06 16:55:21 +0300

    Remove clipboard setting, set expandtab

--- lua/recleunfig/set.lua
+++ lua/recleunfig/set.lua
@@ -1,9 +1,10 @@
 vim.opt.tabstop = 4 -- how many spaces per tab
 vim.opt.shiftwidth = 4 -- how many spaces per indentation
 vim.opt.scrolloff = 8 -- check 24:00 in ThePrimeagen video
+vim.opt.expandtab = true
 
 vim.opt.number = true -- show numbers
 vim.opt.relativenumber = true -- show relative numbers
 vim.opt.wrap = false
-vim.api.nvim_set_option("clipboard", "unnamedplus")
+
 vim.cmd.colorscheme("github_dark")
