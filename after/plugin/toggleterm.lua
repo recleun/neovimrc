d5927f0f134843ee107d016b4b58faedbd1ef4f0
recleun <98621222+recleun@users.noreply.github.com>
2024-09-07 20:11:30 +0300

    Fix lazygit keybind, change normal mode keybind to <C-n>

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -7,11 +7,11 @@ require("toggleterm").setup({
 })
 
 -- keybind to open lazygit
-vim.keymap.set("n", "<C-g>", ":TermExec cmd=lazygit")
+vim.keymap.set("n", "<C-g>", ":TermExec cmd=lazygit<cr>")
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-n>', [[<C-\><C-n>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
